1a69b560e2b0d44252adbb9a680263ef56a16f15
Benchamxd <73537720+Benchamxd@users.noreply.github.com>
2020-11-29 12:32:26 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -57,11 +57,12 @@ $chatid = $update->callback_query->message->chat->id;
 $callback_query = $update->callback_query->data;
 $messageid = $update->callback_query->message->message_id;
 $reply = $update->message->reply_to_message->message_id;
-  $date = date("d/m/y");
-$AdminID = 1222113933; //admin user id get it from @MissRose_bot 
- $log = -1001412381389;
-$jk = -1001237862679;
 //===============BENCHAM=============//
+$tch = json_decode(file_get_contents("https://api.telegram.org/bot".API_KEY."/getChatMember?chat_id=@Cute_baby_gif&user_id=".$from_id))->result->status;
+if($tch != 'member' && $tch != 'creator' && $tch != 'administrator'){
+bot('sendMessage',['chat_id'=>$from_id,'text'=>"Join!"
+]);
+}Else{
 if ($text == "/start"){
 
             bot('sendmessage', [
@@ -97,3 +98,4 @@ bot('copyMessage', [
                
                ]);
         }    
+}
