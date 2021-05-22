b0532c90228181deafd63556d9338d29c43d8a95
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-05-22 20:41:54 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@ error_reporting(0);
 set_time_limit(0);
 
 flush();
-$API_KEY = '1884962279:AAEYQbLCJhRCup97haTuKjps9sE6_V6JEK4'; //Your token
+$API_KEY = '1883437202:AAFD48e8vUKrTWInqbJWg3zv4drPBKXUdfc'; //Your token
 ##------------------------------##
 define('API_KEY',$API_KEY);
 function bot($method,$datas=[]){
